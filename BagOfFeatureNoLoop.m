6981bef1d359d00568350c248e192215e5113a6f
kalyan1998 <31040112+kalyan1998@users.noreply.github.com>
2021-11-08 17:24:19 +0530

    Update BagOfFeatureNoLoop.m

--- BagOfFeatureNoLoop.m
+++ BagOfFeatureNoLoop.m
@@ -1,11 +1,8 @@
-%Data = imageDatastore('Dataset_','IncludeSubfolders',true,'LabelSource','foldernames');
-%Data = imageDatastore('Edgeresults1','IncludeSubfolders',true,'LabelSource','foldernames');
 %Data = imageDatastore('Copy_3_of_Dataset_','IncludeSubfolders',true,'LabelSource','foldernames');
 opts = templateSVM('KernelFunction','polynomial');
 %extractorFcn = @exampleBagOfFeaturesExtractor;
 Data = imageDatastore('Copy_4_of_Dataset_','IncludeSubfolders',true,'LabelSource','foldernames');
-[X,Xnew]=splitEachLabel(Data,0.5);
-%bag = bagOfFeatures(X,'CustomExtractor',extractorFcn,"VocabularySize",638);
+[X,Xnew]=splitEachLabel(Data,0.4);
 bag = bagOfFeatures(X,'StrongestFeatures',1,"VocabularySize",638,'GridStep',[1 1],'UseParallel',true);
 categoryClassifier = trainImageCategoryClassifier(X,bag,'LearnerOptions',opts,'UseParallel',true);
 confMatrix = evaluate(categoryClassifier,Xnew);
