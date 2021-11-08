75fd2e619bfed4189728431da9c80eef77d53982
kalyan1998 <31040112+kalyan1998@users.noreply.github.com>
2021-11-08 17:24:34 +0530

    Update BagOfFeatureNoLoop.m

--- BagOfFeatureNoLoop.m
+++ BagOfFeatureNoLoop.m
@@ -1,6 +1,5 @@
 %Data = imageDatastore('Copy_3_of_Dataset_','IncludeSubfolders',true,'LabelSource','foldernames');
 opts = templateSVM('KernelFunction','polynomial');
-%extractorFcn = @exampleBagOfFeaturesExtractor;
 Data = imageDatastore('Copy_4_of_Dataset_','IncludeSubfolders',true,'LabelSource','foldernames');
 [X,Xnew]=splitEachLabel(Data,0.4);
 bag = bagOfFeatures(X,'StrongestFeatures',1,"VocabularySize",638,'GridStep',[1 1],'UseParallel',true);
