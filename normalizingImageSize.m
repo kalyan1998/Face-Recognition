84c5dee128076612d71dfcdca75950d5ab6093aa
kalyan1998 <31040112+kalyan1998@users.noreply.github.com>
2021-11-08 17:27:34 +0530

    Update normalizingImageSize.m

--- normalizingImageSize.m
+++ normalizingImageSize.m
@@ -4,18 +4,18 @@ k=1;
 for n=1:numel(Data.Files)
     I = imread(Data.Files{n});
     I1=imresize(I,[200 200]);
-%     NoseDetect = vision.CascadeObjectDetector('Nose','MergeThreshold',64);
-%     BB=step(NoseDetect,I1);
-%     if(numel(BB)~=0)
-%        [nose,rect]=imcrop(I1,BB(1,:));
-%        Iblur = imgaussfilt(I1(rect(2):rect(2)+rect(4),rect(1):rect(1)+rect(3),:),100);
-%        I1(rect(2):rect(2)+rect(4),rect(1):rect(1)+rect(3),:)=Iblur;
-%        imwrite(I1,Data.Files{n});
-%    else
-%        l{k}=Data.Files{n};
-%        k=k+1;
-%        imwrite(I1,Data.Files{n});
-%        disp("nose not detected"+" "+Data.Files{n});
-%     end
+    NoseDetect = vision.CascadeObjectDetector('Nose','MergeThreshold',64);
+    BB=step(NoseDetect,I1);
+    if(numel(BB)~=0)
+        [nose,rect]=imcrop(I1,BB(1,:));
+        Iblur = imgaussfilt(I1(rect(2):rect(2)+rect(4),rect(1):rect(1)+rect(3),:),100);
+        I1(rect(2):rect(2)+rect(4),rect(1):rect(1)+rect(3),:)=Iblur;
+        imwrite(I1,Data.Files{n});
+    else
+        l{k}=Data.Files{n};
+        k=k+1;
+        imwrite(I1,Data.Files{n});
+        disp("nose not detected"+" "+Data.Files{n});
+    end
     imwrite(I1,Data.Files{n});
 end
